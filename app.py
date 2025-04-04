4dd153ef26da85832dfa893cebcdef169a4f2e60
Marishel <marishel.lochan@my.uwi.edu>
2025-04-04 00:16:23 -0400

    Fix files

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def get_project2():
 
 @app.route('/airbnbProject', methods=['GET'])
 def get_project3():
-    return redirect('static/Airbnb_Listings.html')
+    return redirect('Airbnb_Listings.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
