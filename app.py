8e47ed1c9c8b6840f6ecd909a2b8ca28babe267b
Marishel <marishel.lochan@my.uwi.edu>
2025-04-03 22:50:21 -0400

    Updated html

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
-from flask import Flask, send_file
+from flask import Flask, render_template, send_file, redirect
 
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('webpage.html')
 
 @app.route('/Project- Investigating Netflix Movies - DataCamp.pdf', methods=['GET'])
 def get_project1():
     return send_file('static/Project- Investigating Netflix Movies - DataCamp.pdf', as_attachment=False)
 
-@app.route('/project2.pdf', methods=['GET'])
+@app.route('/excelProject', methods=['GET'])
 def get_project2():
-    return send_file('project2.pdf', as_attachment=False)
+    return redirect('https://github.com/marishellochan/Financial_Ratios_Trend_Database_Template')
+
+@app.route('/airbnbProject', methods=['GET'])
+def get_project3():
+    return redirect('static/Airbnb_Listings.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
